d4850a742c35be6213b9582fc8e06506277f163c
DESKTOP-KRSTE\toshe <toshev.krste@yahoo.com>
2025-02-09 18:12:13 +0100

    Fix image mode. Set aspect.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request
-from request import retrieve_quote, retrieve_photo
+from flask import Flask, render_template
+import request
 
 app = Flask(__name__)
 
-quote_of_the_day = retrieve_quote()
-retrieve_photo()
+quote_of_the_day = request.retrieve_quote()
+photo_of_the_day = request.retrieve_photo()
 
 @app.route("/")
 def home():
-    return render_template("index.html", image="static\img.jpg", quote=quote_of_the_day)
+    return render_template("index.html", image="static/images/img.jpg", quote=quote_of_the_day)
 
 if __name__ == "__main__":
     app.run()
